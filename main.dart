73b9337688cf867ccddb2f57f9d7471557d84dd5
Łukasz Bieniek <woramowy@gmail.com>
2020-05-27 17:07:46 +0200

    Removal of irrelevant comments

--- main.dart
+++ main.dart
@@ -5,18 +5,6 @@ import 'game/Game.dart';
 import 'locations/Location.dart';
 
 void main() {
-  // Fox robert = new Fox('Robert', new Location('void'));
-  // Fox marcin = new Fox('Marcin', new Location('void'));
-
-  // print('Start fight!');
-  // Fox winner = Fight(marcin, robert).doFight((int damage, Animals whoLost) {
-  //   if(damage == 0) {
-  //     print('${whoLost.name} dodge the atack!');
-  //   } else {
-  //     print('${whoLost.name} lost: $damage hp! ${whoLost.name} has ${whoLost.acctualHp} HP');
-  //   }
-  // });
-  // print('Winner is: ${winner.name}!! Congratulations!!!');
   Game game = new Game();
   while (!game.isExit) {
     game.nextTurn();
