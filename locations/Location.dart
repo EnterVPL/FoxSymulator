9171245b7c67b73ac1aa45dadf27b5b0769ae425
EnterVPL <woramowy@gmail.com>
2020-08-29 17:16:07 +0200

    Add 4 spaces formating

--- locations/Location.dart
+++ locations/Location.dart
@@ -3,31 +3,31 @@ import '../game/Actions.dart';
 
 class Location
 {
-  final String name;
-  List<Animals> animalList = List<Animals>();
-  List<Actions> actions = List<Actions>();
+    final String name;
+    List<Animals> animalList = List<Animals>();
+    List<Actions> actions = List<Actions>();
 
-  Location(this.name);
+    Location(this.name);
 
-  void addAnimal(Animals animal) {
-    animalList.add(animal);
-  }
+    void addAnimal(Animals animal) {
+        animalList.add(animal);
+    }
 
-  void removeAnimal(int index) {
-    animalList.removeAt(index);
-  }
+    void removeAnimal(int index) {
+        animalList.removeAt(index);
+    }
 
-  @override
-  String toString() {
-    return name;
-  }
+    @override
+    String toString() {
+        return name;
+    }
 
-  void addAction(Actions action) {
-    actions.add(action);
-  }
+    void addAction(Actions action) {
+        actions.add(action);
+    }
 
-  List<Actions> getActiveActions() {
-    return actions.where((action) => action.isActive);
-  }
+    List<Actions> getActiveActions() {
+        return actions.where((action) => action.isActive);
+    }
 
 }
