6c92389c6b1937168606f653f6c933f4c9c60887
EnterVPL <woramowy@gmail.com>
2020-12-24 22:42:14 +0100

    Start translate animals

--- animals/Animals.dart
+++ animals/Animals.dart
@@ -1,53 +1,59 @@
 import 'dart:math';
 
-class Animals
-{
-    String name;
-    int acctualHp;
-    int maxHp;
-    int speed;
-    int strengh;
-    int defence;
-    bool isLive = true;
+import '../langs/Language.dart';
+import '../langs/LanguagesTypes.dart';
 
-    Animals create(String name, int maxHp, int speed, int strengh, int defence) {
-        Animals animal = new Animals();
-        animal.name = name;
-        animal.maxHp = animal.acctualHp = maxHp;
-        animal.speed = speed;
-        animal.strengh = strengh;
-        animal.defence = defence;
-        return animal;
+class Animals {
+  String _name;
+  int acctualHp;
+  int maxHp;
+  int speed;
+  int strengh;
+  int defence;
+  bool isLive = true;
+
+  String get name {
+    return Language.getTranslation(LanguagesTypes.ANIMALS, name);
+  }
+
+  Animals create(String name, int maxHp, int speed, int strengh, int defence) {
+    Animals animal = new Animals();
+    animal._name = name;
+    animal.maxHp = animal.acctualHp = maxHp;
+    animal.speed = speed;
+    animal.strengh = strengh;
+    animal.defence = defence;
+    return animal;
+  }
+
+  int atack(Animals enemy) {
+    int hitChance = calcHitChance(this.speed, enemy.speed);
+    Random rnd = Random();
+    int damage = 0;
+    if (hitChance >= rnd.nextInt(200)) {
+      damage = doDamage(this, enemy, rnd);
+    }
+    return damage;
+  }
+
+  int calcHitChance(int atackerSpeed, int defendingSpeed) {
+    return ((atackerSpeed.toDouble() / defendingSpeed.toDouble()) * 100)
+        .toInt();
+  }
+
+  int doDamage(Animals atacker, Animals defending, Random rnd) {
+    int maxDamage = 0;
+    maxDamage = atacker.strengh - defending.defence;
+    if (maxDamage <= 0) {
+      maxDamage = 1;
     }
 
-    int atack(Animals enemy) {
-        int hitChance = calcHitChance(this.speed, enemy.speed);
-        Random rnd = Random();
-        int damage = 0;
-        if(hitChance >= rnd.nextInt(200)) {
-            damage = doDamage(this, enemy, rnd);
-        }
-        return damage;
+    int damage = rnd.nextInt(maxDamage + 1);
+    defending.acctualHp -= damage;
+    if (defending.acctualHp <= 0) {
+      defending.acctualHp = 0;
+      defending.isLive = false;
     }
-
-    int calcHitChance(int atackerSpeed, int defendingSpeed) {
-        return ((atackerSpeed.toDouble() / defendingSpeed.toDouble()) * 100).toInt();
-    }
-
-    int doDamage(Animals atacker, Animals defending, Random rnd) {
-        int maxDamage = 0;
-        maxDamage = atacker.strengh - defending.defence;
-        if(maxDamage <= 0) {
-            maxDamage = 1;
-        }
-    
-        int damage = rnd.nextInt(maxDamage+1);
-        defending.acctualHp -= damage;
-        if(defending.acctualHp <= 0) {
-            defending.acctualHp = 0;
-            defending.isLive = false;
-        }
-        return damage;
-    } 
-
+    return damage;
+  }
 }
