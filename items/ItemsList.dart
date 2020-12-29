b28ef70f7839f253eb494d336f61f6314dfebbc9
EnterVPL <woramowy@gmail.com>
2020-12-29 17:07:21 +0100

    add more items

--- items/ItemsList.dart
+++ items/ItemsList.dart
@@ -14,7 +14,7 @@ class ItemsList {
    *  }
    * ]
    */
-  static getIt() {
+  static Map<int, List<Map<String, dynamic>>> getIt() {
     return {
       ItemTypes.FOOD: [
         {
@@ -55,6 +55,72 @@ class ItemsList {
             StatsType.ACCTUALHP: -2,
             StatsType.SPEED: 4
           }
+        },
+      ],
+      ItemTypes.SHIELD: [
+        {
+          'id': 0,
+          'name': '{Weak shield}',
+          'benefits': {StatsType.DEFENCE: 1}
+        },
+        {
+          'id': 1,
+          'name': '{Normal shield}',
+          'benefits': {StatsType.DEFENCE: 2}
+        },
+        {
+          'id': 2,
+          'name': '{Energy shield}',
+          'benefits': {StatsType.DEFENCE: 3, StatsType.SPEED: 1}
+        },
+        {
+          'id': 3,
+          'name': '{Ironing board}',
+          'benefits': {StatsType.DEFENCE: 4, StatsType.SPEED: 2}
+        },
+      ],
+      ItemTypes.ARMMOR: [
+        {
+          'id': 0,
+          'name': '{Weak Armmor}',
+          'benefits': {StatsType.DEFENCE: 1}
+        },
+        {
+          'id': 1,
+          'name': '{Normal Armmor}',
+          'benefits': {StatsType.DEFENCE: 2}
+        },
+        {
+          'id': 2,
+          'name': '{Energy Armmor}',
+          'benefits': {StatsType.DEFENCE: 3, StatsType.SPEED: 1}
+        },
+        {
+          'id': 3,
+          'name': '{Pan Armmor}',
+          'benefits': {StatsType.DEFENCE: 4, StatsType.SPEED: 2}
+        },
+      ],
+      ItemTypes.WEAPON: [
+        {
+          'id': 0,
+          'name': '{Weak Weapon}',
+          'benefits': {StatsType.STRENGH: 1}
+        },
+        {
+          'id': 1,
+          'name': '{Normal Weapon}',
+          'benefits': {StatsType.STRENGH: 2}
+        },
+        {
+          'id': 2,
+          'name': '{Energy Weapon}',
+          'benefits': {StatsType.STRENGH: 3, StatsType.SPEED: 1}
+        },
+        {
+          'id': 3,
+          'name': "{Dragon's breath sword}",
+          'benefits': {StatsType.STRENGH: 4, StatsType.SPEED: 2}
         },
       ]
     };
