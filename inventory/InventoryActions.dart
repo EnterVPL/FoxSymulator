5062282f8c7527110541a3f755bb7348788ed346
EnterVPL <woramowy@gmail.com>
2021-01-06 01:29:13 +0100

    fix wear items

--- inventory/InventoryActions.dart
+++ inventory/InventoryActions.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import '../animals/Stats.dart';
 import '../game/Game.dart';
 import '../items/Item.dart';
@@ -66,74 +64,7 @@ class InventoryActions {
   static void useItem(int itemType, int itemId, Inventory inventor) {
     if (inventor.items[itemType][itemId].count > 0) {
       if (inventor.items[itemType][itemId].type != ItemTypes.FOOD) {
-        switch (inventor.items[itemType][itemId].type) {
-          case ItemTypes.ARMMOR:
-            Game.hero.usingArmmor;
-            if (Game.hero.usingArmmor != null) {
-              if (Game.hero.usingArmmor.id ==
-                  inventor.items[itemType][itemId].id) break;
-              Game.hero.usingArmmor.benefits.forEach((type, value) {
-                Game.hero.stats[type] -= value;
-              });
-              Game.hero.usingArmmor.isWear = false;
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingArmmor = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            } else {
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingArmmor = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            }
-            break;
-          case ItemTypes.SHIELD:
-            Game.hero.usingShield;
-            if (Game.hero.usingShield != null) {
-              if (Game.hero.usingShield.id ==
-                  inventor.items[itemType][itemId].id) break;
-              Game.hero.usingShield.benefits.forEach((type, value) {
-                Game.hero.stats[type] -= value;
-              });
-              Game.hero.usingShield.isWear = false;
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingShield = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            } else {
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingShield = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            }
-            break;
-          case ItemTypes.WEAPON:
-            Game.hero.usingWeapon;
-            if (Game.hero.usingWeapon != null) {
-              if (Game.hero.usingWeapon.id ==
-                  inventor.items[itemType][itemId].id) break;
-              Game.hero.usingWeapon.benefits.forEach((type, value) {
-                Game.hero.stats[type] -= value;
-              });
-              Game.hero.usingWeapon.isWear = false;
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingWeapon = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            } else {
-              inventor.items[itemType][itemId].isWear = true;
-              Game.hero.usingWeapon = inventor.items[itemType][itemId];
-              inventor.items[itemType][itemId].benefits.forEach((type, value) {
-                Game.hero.stats[type] += value;
-              });
-            }
-            break;
-        }
+        wearItem(inventor.items[itemType][itemId]);
       } else {
         inventor.items[itemType][itemId].count -= 1;
         inventor.items[itemType][itemId].benefits.forEach((type, value) {
@@ -145,9 +76,22 @@ class InventoryActions {
         Game.hero.stats[StatsType.MAXHP]) {
       Game.hero.stats[StatsType.ACCTUALHP] = Game.hero.stats[StatsType.MAXHP];
     }
-    if (Game.hero.stats[StatsType.SATIETY] >
-        Game.hero.minMaxComfort.reduce(max)) {
-      Game.hero.stats[StatsType.SATIETY] = Game.hero.minMaxComfort.reduce(max);
+    if (Game.hero.stats[StatsType.SATIETY] > Game.hero.comfort) {
+      Game.hero.stats[StatsType.SATIETY] = Game.hero.comfort;
+    }
+    if (Game.hero.stats[StatsType.ENERGY] > Game.hero.comfort) {
+      Game.hero.stats[StatsType.ENERGY] = Game.hero.comfort;
+    }
+  }
+
+  static void wearItem(Item wearingItem) {
+    if (Game.hero.usingItems.containsKey(wearingItem.type)) {
+      Game.hero.usingItems[wearingItem.type].isWear = false;
+      Game.hero.usingItems[wearingItem.type] = wearingItem;
+      Game.hero.usingItems[wearingItem.type].isWear = true;
+    } else {
+      Game.hero.usingItems.putIfAbsent(wearingItem.type, () => wearingItem);
+      Game.hero.usingItems[wearingItem.type].isWear = true;
     }
   }
 }
